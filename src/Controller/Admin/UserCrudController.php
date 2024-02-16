4834300d609dd9befad0c70f4a4f6e4a5c0e33dc
SihamWeb <siham.charef@etu.unilim.fr>
2024-02-16 22:29:14 +0100

    master: Add a role admin to an user in easyadmin && debug hash password

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -7,7 +7,10 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -22,7 +25,20 @@ class UserCrudController extends AbstractCrudController
             TextField::new('email'),
             TextField::new('firstname'),
             TextField::new('lastname'),
-            ChoiceField::new('roles')
+            TextField::new('password')
+            ->setFormType(RepeatedType::class)
+            ->hideOnIndex()
+            ->setFormTypeOptions([
+                'type' => PasswordType::class,
+                'first_options' => [
+                'label' => 'Password',
+                'hash_property_path' => 'password',
+                ],
+                'second_options' => ['label' => '(Repeat)'],
+                'mapped' => false,
+                ])
+                ->onlyOnForms(),
+            ArrayField::new('roles', 'Roles'),
         ];
     }
 }
