df48c74ce937e4313c4fe161626133924e5fbcd7
Dylan Louichon <dylanlouichon@gmail.com>
2024-02-18 16:09:56 +0100

    Update MainController : Recover data from last three articles and  pass it to the view

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
+
+        $articles = $articleRepository->findBy([], ['createdAt' => 'DESC'], 3);
+        
         return $this->render('main/main.html.twig', [
             'controller_name' => 'MainController',
+            'articles' => $articles,
         ]);
     }
 }
